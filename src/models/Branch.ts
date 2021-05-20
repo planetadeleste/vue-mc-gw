997d41f478665ccb87f7d52b06d5dd4fb8b5be75
Alvaro Canepa <bfpdevel@gmail.com>
2021-05-20 10:56:56 -0300

    - Add defaults properties to models

--- src/models/Branch.ts
+++ src/models/Branch.ts
@@ -5,13 +5,27 @@ export default class Branch extends Model {
   defaults(): Record<string, any> {
     return {
       id: null,
+      company_id: null,
+      country_id: null,
+      state_id: null,
+      town_id: null,
+      active: null,
+      is_default: null,
       name: null,
+      code: null,
+      address: null,
+      phone: null,
+      email: null,
     };
   }
 
   mutations(): Record<string, any> {
     return {
       id: (id: string) => toNumber(id) || null,
+      company_id: (id: string) => toNumber(id) || null,
+      country_id: (id: string) => toNumber(id) || null,
+      state_id: (id: string) => toNumber(id) || null,
+      town_id: (id: string) => toNumber(id) || null,
     };
   }
 
--- src/models/Company.ts
+++ src/models/Company.ts
@@ -5,12 +5,27 @@ export default class Company extends Model {
   defaults(): Record<string, any> {
     return {
       id: null,
+      name: null,
+      active: true,
+      slug: null,
+      code: null,
+      description: null,
+      preview_image: null,
+      doc_id: null,
+      doc_type: null,
+      address: null,
+      country_id: null,
+      state_id: null,
+      town_id: null,
     };
   }
 
   mutations(): Record<string, any> {
     return {
       id: (id: string) => toNumber(id) || null,
+      country_id: (id: string) => toNumber(id) || null,
+      state_id: (id: string) => toNumber(id) || null,
+      town_id: (id: string) => toNumber(id) || null,
     };
   }
 
