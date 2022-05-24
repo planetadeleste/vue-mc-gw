bc2b3f104d934881581a1593177ba93fa859cc9e
Alvaro Canepa <bfpdevel@gmail.com>
2022-05-24 05:01:36 -0300

    v1.0.63 - Fill settings properties

--- src/types/Settings.d.ts
+++ src/types/Settings.d.ts
@@ -1,3 +1,12 @@
 export interface SettingsData {
   preview_image: string;
+  currency_ui: number;
+  pricelist_label: string;
+  max_discounts: number;
+  invoice_ui: MaxInvoicesUI[];
+}
+
+export interface MaxInvoicesUI {
+  date: string;
+  value: number;
 }
